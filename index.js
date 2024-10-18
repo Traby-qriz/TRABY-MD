de722861c6903ea0ea42184582147773b97590e8
Traby-qriz <146692830+Traby-qriz@users.noreply.github.com>
2024-10-18 13:58:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,42 @@
 
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const autoStatus = require('./commands/autoStatus.js');
-const autoReact = require('./commands/autoReact.js');
-const antiDelete = require('./commands/antiDelete.js');
-const googleSearch = require('./commands/googleSearch.js');
-// Import other commands as needed
+
+const autoStatus = require('./commands/autoStatus');
+const autoReact = require('./commands/autoReact');
+const antiDelete = require('./commands/antiDelete');
+const googleSearch = require('./commands/googleSearch');
+const jokesMaker = require('./commands/jokesMaker');
+const stickerMaker = require('./commands/stickerMaker');
+const riddlesMaker = require('./commands/riddlesMaker');
+const youtubeDownloader = require('./commands/youtubeDownloader');
+const songDownloader = require('./commands/songDownloader');
+const ownerInfo = require('./commands/ownerInfo');
 
 const client = new Client();
 
-client.on('qr', (qr) => {
+// Generate QR code for authentication
+client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
 });
 
+// Bot ready
 client.on('ready', () => {
     console.log('TRABY-MD is ready!');
 });
 
+// Handle incoming messages
 client.on('message', message => {
     autoStatus(client, message);
     autoReact(client, message);
     antiDelete(client, message);
     googleSearch(client, message);
-    // Call other command handlers similarly
+    jokesMaker(client, message);
+    stickerMaker(client, message);
+    riddlesMaker(client, message);
+    youtubeDownloader(client, message);
+    songDownloader(client, message);
+    ownerInfo(client, message);
 });
 
 // Start the client
