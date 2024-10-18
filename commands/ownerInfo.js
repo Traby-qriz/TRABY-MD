9407d2452ebaef5eafa1a34a1f27b8bc8826969a
Traby-qriz <146692830+Traby-qriz@users.noreply.github.com>
2024-10-18 15:43:46 +0300

    Update ownerInfo.js

--- commands/ownerInfo.js
+++ commands/ownerInfo.js
@@ -2,6 +2,6 @@
 
 module.exports = (client, message) => {
     if (message.body.startsWith('!owner')) {
-        message.reply(`Owner info: Name: Your Name, Contact: Your Contact Info`);
+        message.reply(`Owner info: Name: Traby qriz, Contact: 254732982940 `);
     }
 };
